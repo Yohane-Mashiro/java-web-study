9ac5bd9788161e35d8da3310d40bc27c571b92d0
晨曦 <78677516+Yohane-Mashiro@users.noreply.github.com>
2025-03-31 21:11:55 +0800

    2025/3/31

--- study_1/src/main/java/newnew/study/Error.java
+++ study_1/src/main/java/newnew/study/Error.java
@@ -1,9 +1,11 @@
 package newnew.study;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 
 public class Error extends HttpServlet {
