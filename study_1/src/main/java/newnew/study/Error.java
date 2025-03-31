1afb98ed688a39c5277f1f071a0a145fee1e1895
晨曦 <78677516+Yohane-Mashiro@users.noreply.github.com>
2025-03-31 21:18:06 +0800

    2025/3/31

--- study_1/src/main/java/newnew/study/Error.java
+++ study_1/src/main/java/newnew/study/Error.java
@@ -11,11 +11,13 @@ import java.io.IOException;
 public class Error extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        System.out.println("error");
+        resp.getWriter().write("error");
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        System.out.println("error");
+        resp.getWriter().write("error");
     }
 }
